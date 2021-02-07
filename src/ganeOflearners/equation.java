dd89ec8041cff4a886f83728156a56d87d752a5e
Jacob Mbuco <gichira44@gmail.com>
2021-02-06 23:42:34 -0800

    getting values from the user

--- src/ganeOflearners/equation.java
+++ src/ganeOflearners/equation.java
@@ -1,9 +1,19 @@
 package ganeOflearners;
+import java.util.Scanner;
 
 public class equation {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//getting values from the user 
+				//values cannot be zero because it will turn the quadratic equation into linear
+				Scanner values = new Scanner (System.in);
+				System.out.print("Enter the value of a: " );
+				double anum=values.nextDouble();
+				System.out.print("Enter the value of b: " );
+				double bnum=values.nextDouble();
+				System.out.print("Enter the value of c: " );
+				double cnum=values.nextDouble();
 
 	}
 
